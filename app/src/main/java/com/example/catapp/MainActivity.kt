644cb426f2260156ec40bbc1cfc2b4d6c5e03d7d
Diogo <l55466@alunos.uevora.pt>
2025-08-12 14:27:40 +0100

    fix:indentation

--- app/src/main/java/com/example/catapp/MainActivity.kt
+++ app/src/main/java/com/example/catapp/MainActivity.kt
@@ -28,13 +28,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}data class Cat(
-    val id: String,
-    val url: String,
-    val width: Int,
-    val height: Int
-)
-
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
