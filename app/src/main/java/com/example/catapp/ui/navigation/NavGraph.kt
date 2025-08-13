67c7d8eed5df177c5cfc497d109dd4d9e0036df6
Diogo <l55466@alunos.uevora.pt>
2025-08-13 19:43:03 +0100

    feat: add details screen to navigation

--- app/src/main/java/com/example/catapp/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/example/catapp/ui/navigation/NavGraph.kt
@@ -10,7 +10,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.*
+import androidx.navigation.navArgument
+import com.example.catapp.ui.screens.CatDetailsScreen
 import com.example.catapp.ui.screens.FavouritesScreen
 import com.example.catapp.ui.screens.HomeScreen
 import com.example.catapp.viewmodel.FavoritesViewModel
@@ -26,7 +29,7 @@ sealed class Screen(val route: String, val label: String, val icon: ImageVector)
 fun NavGraph() {
     val navController = rememberNavController()
     val favoritesViewModel: FavoritesViewModel = viewModel()
-    val homeScreenViewModel : HomeScreenViewModel = viewModel()
+    val homeScreenViewModel: HomeScreenViewModel = viewModel()
     val screens = listOf(Screen.Home, Screen.Favourites)
 
     // Scaffold with bottom navigation bar
@@ -66,15 +69,36 @@ fun NavGraph() {
         ) {
             // Home screen route
             composable(Screen.Home.route) {
-                HomeScreen(favoritesViewModel = favoritesViewModel, homeScreenViewModel = homeScreenViewModel, navController = navController)
+                HomeScreen(
+                    favoritesViewModel = favoritesViewModel,
+                    homeScreenViewModel = homeScreenViewModel,
+                    navController = navController
+                )
             }
 
             // Favourites list screen route
             composable(Screen.Favourites.route) {
-                FavouritesScreen(navController = navController, homeScreenViewModel = homeScreenViewModel,favoritesViewModel = favoritesViewModel)
+                FavouritesScreen(
+                    navController = navController,
+                    homeScreenViewModel = homeScreenViewModel,
+                    favoritesViewModel = favoritesViewModel
+                )
             }
 
+            // Breed details route
+            composable(
+                "breedDetails/{breedId}",
+                arguments = listOf(navArgument("breedId") { type = NavType.StringType })
+            ) { backStackEntry ->
+                val breedId = backStackEntry.arguments?.getString("breedId") ?: ""
+                CatDetailsScreen(
+                    breedId = breedId,
+                    homeScreenViewModel = homeScreenViewModel,
+                    favoritesViewModel = favoritesViewModel,
+                    navController = navController
+                )
             }
         }
     }
+}
 
